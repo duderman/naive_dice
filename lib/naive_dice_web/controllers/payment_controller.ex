85fb00e4de38d863e7a38e434d1b2d6e73da657a
Nikolay Norkin <nikolay.norkin@forward3d.com>
2020-03-13 22:12:20 +0000

    Use fallback in payments controller

--- lib/naive_dice_web/controllers/payment_controller.ex
+++ lib/naive_dice_web/controllers/payment_controller.ex
@@ -5,19 +5,21 @@ defmodule NaiveDiceWeb.PaymentController do
 
   def process(conn, %{"checkout_session_id" => checkout_session_id}) do
     with {:ok, ticket} <- Tickets.get_by_checkout_session_id(checkout_session_id),
-         {:ok, status} <- Stripe.CheckoutSession.StatusChecker.check(checkout_session_id),
-         {:ok, %{id: id}} <- Tickets.update_status(ticket, status) do
-      redirect(conn, to: Routes.ticket_path(conn, :show, id))
-    else
-      {:error, :canceled, %{event_id: event_id}} ->
-        conn
-        |> put_flash(:error, "Purchase was cancelled")
-        |> redirect(to: Routes.event_path(conn, :show, event_id))
+         {:ok, status} <- Stripe.CheckoutSession.StatusChecker.check(checkout_session_id) do
+      case Tickets.update_status(ticket, status) do
+        {:ok, %{id: id}} ->
+          redirect(conn, to: Routes.ticket_path(conn, :show, id))
 
-      {:error, :unknown_status, %{id: id}} ->
-        conn
-        |> put_flash(:error, "Something went wrong with the payment. Try again Please")
-        |> redirect(to: Routes.ticket_ticket_path(conn, :checkout, id))
+        {:error, :canceled, %{event_id: event_id}} ->
+          conn
+          |> put_flash(:error, "Purchase was cancelled")
+          |> redirect(to: Routes.event_path(conn, :show, event_id))
+
+        {:error, :unknown_status, %{id: id}} ->
+          conn
+          |> put_flash(:error, "Something went wrong with the payment. Try again Please")
+          |> redirect(to: Routes.ticket_ticket_path(conn, :checkout, id))
+      end
     end
   end
 end
