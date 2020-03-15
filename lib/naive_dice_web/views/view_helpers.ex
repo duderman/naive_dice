301d5ecb7c2ef2706a0e7ece00cdb65058a545cd
Nikolay Norkin <nikolay.norkin@forward3d.com>
2020-03-15 14:42:28 +0000

    pluralize

--- lib/naive_dice_web/views/view_helpers.ex
+++ lib/naive_dice_web/views/view_helpers.ex
@@ -5,4 +5,7 @@ defmodule NaiveDiceWeb.ViewHelpers do
   def back_to_root_link do
     link("← back to root", to: event_path(NaiveDiceWeb.Endpoint, :index))
   end
+
+  def pluralize(word, 1), do: word
+  def pluralize(word, _), do: word <> "s"
 end
