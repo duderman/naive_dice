8839dbbe64342c77b328517961520cd21ac03158
Nikolay Norkin <nikolay.norkin@forwardpmx.com>
2020-08-03 15:03:39 +0100

    Dynamic api key loading

--- lib/stripe/checkout_session/api.ex
+++ lib/stripe/checkout_session/api.ex
@@ -2,14 +2,11 @@ defmodule Stripe.CheckoutSession.Api do
   @moduledoc """
   Provides Stripe API requests
   """
-  @api_key Application.get_env(:naive_dice, Stripe)[:api_key]
-
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://api.stripe.com/v1/checkout/sessions"
   plug Tesla.Middleware.JSON
   plug Tesla.Middleware.FormUrlencoded
-  plug Tesla.Middleware.BasicAuth, username: @api_key
 
   @spec create(%{} | String.t()) :: Tesla.Env.result()
   def create(payload) when is_map(payload) do
@@ -20,11 +17,21 @@ defmodule Stripe.CheckoutSession.Api do
   end
 
   def create(payload) do
-    post("", payload)
+    new() |> post("", payload)
   end
 
   @spec fetch(String.t()) :: Tesla.Env.result()
   def fetch(id) do
-    get(id, query: ["expand[]": "payment_intent"])
+    new() |> get(id, query: ["expand[]": "payment_intent"])
+  end
+
+  defp new() do
+    Tesla.client [
+      {Tesla.Middleware.BasicAuth, %{username: api_key()}}
+    ]
+  end
+
+  defp api_key do
+    Application.get_env(:naive_dice, Stripe)[:api_key]
   end
 end
